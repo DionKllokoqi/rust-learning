11d3da744e2fd86e94cf3f70ac4ffcce8fc5c90e
Dion Kllokoqi <dion.kllokoqi@gmail.com>
2026-02-12 14:55:42 +0100

    chore: fix grammar

--- types/src/main.rs
+++ types/src/main.rs
@@ -134,7 +134,7 @@ fn main() {
 
     let a = [1, 2, 3, 4, 5];
 
-    // Arrays are useful when you want your data allocated on the stack, the same as the other types we have seen so far, rather than the heap, or, when when you want to ensure you always have a fixed number of elements.
+    // Arrays are useful when you want your data allocated on the stack, the same as the other types we have seen so far, rather than the heap, or, when you want to ensure you always have a fixed number of elements.
 
     // An array isn’t as flexible as the vector type, though. A vector is a similar collection type provided by the standard library that is allowed to grow or shrink in size because its contents live on the heap.
 
